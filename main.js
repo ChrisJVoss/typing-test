a1b3c8c07e4cca20223983ab4094108511bb692c
Chris Voss <chrisvoss92@gmail.com>
2017-06-12 10:28:00 -0700

    User can see prompt for current letter.

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 /* eslint-disable no-unused-vars */
 
-var text = document.querySelector('#text').textContent
+var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+var splitText = text.split('')
+var $span = document.querySelector('#text')
 
-
-
-function currentLetter() {
+for (var i = 0; i < splitText.length; i++) {
+  var $text = document.createElement('span')
+  $text.textContent = splitText[i]
+  $text.className = ('id')
+  $span.appendChild($text)
 }
+
+var $textList = document.querySelectorAll('span.id')
+var i = 0
+
+document.addEventListener('DOMContentLoaded', function() {
+  $textList[0].classList.toggle('bold')
+})
+
+document.addEventListener('keydown', function(event) {
+  if (i < $textList.length) {
+    $textList[i + 1].classList.toggle('bold')
+  }
+  i += 1
+})
