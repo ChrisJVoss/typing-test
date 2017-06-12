4ea6522f698c78d6822f37190cdd15363ce73f0d
Chris Voss <chrisvoss92@gmail.com>
2017-06-12 13:38:14 -0700

    User can complete a test adn view a score.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 
-var text = "Lorem ipsum."
+var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
 var splitText = text.split('')
 var $span = document.querySelector('#text')
 
@@ -19,18 +19,30 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 document.addEventListener('keydown', function(event) {
-  if (event.keyCode === 16) {
-    return false
+  if (i < $textList.length) {
+    if (event.keyCode === 16) {
+      return false
+    }
+    if (i < $textList.length - 1) {
+      $textList[i + 1].classList.toggle('bold')
+    }
+    if (event.key === $textList[i].textContent) {
+      $textList[i].classList.toggle('correct')
+    }
+    else if (event.key !== $textList[i].textContent) {
+      $textList[i].classList.toggle('incorrect')
+    }
+    $textList[i].classList.toggle('bold')
+    i += 1
   }
-  if (i < $textList.length - 1) {
-    $textList[i + 1].classList.toggle('bold')
+  else {
+    if (document.querySelectorAll('div.score').length === 0) {
+      var $correct = document.querySelectorAll('span.correct')
+      var score = ($correct.length /  $textList.length) * 100
+      var $score = document.createElement('div')
+      $score.className = 'score'
+      $score.textContent = 'Your score is ' + score + '%!'
+      document.body.appendChild($score)
+    }
   }
-  if (event.key === $textList[i].textContent) {
-    $textList[i].classList.toggle('correct')
-  }
-  else if (event.key !== $textList[i].textContent) {
-    $textList[i].classList.toggle('incorrect')
-  }
-  $textList[i].classList.toggle('bold')
-  i += 1
 })
