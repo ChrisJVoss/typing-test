b76c73ea7d8752efa2ab2986f2c1e05b3d4c8348
Chris Voss <chrisvoss92@gmail.com>
2017-06-12 12:14:53 -0700

    User can type a letter and get feedback.

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-unused-vars */
 
-var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+var text = "Lorem ipsum." // " dolor sit amet, consectetur adipiscing elit."
 var splitText = text.split('')
 var $span = document.querySelector('#text')
 
 for (var i = 0; i < splitText.length; i++) {
   var $text = document.createElement('span')
   $text.textContent = splitText[i]
-  $text.className = 'character'
+  $text.className = ('id')
   $span.appendChild($text)
 }
 
-var $textList = document.querySelectorAll('span.character')
+var $textList = document.querySelectorAll('span.id')
 var i = 0
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -19,8 +19,21 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 document.addEventListener('keydown', function(event) {
-  if (i < $textList.length) {
+  if (event.keyCode === 16) {
+    return false
+  }
+  if (i < $textList.length - 1) {
     $textList[i + 1].classList.toggle('bold')
   }
+  if (event.key === $textList[i].textContent) {
+    $textList[i].classList.toggle('correct')
+  }
+  else if (event.key !== $textList[i].textContent) {
+    $textList[i].classList.toggle('incorrect')
+  }
+  $textList[i].classList.toggle('bold')
   i += 1
+  if (i === $textList.length) {
+    return
+  }
 })
